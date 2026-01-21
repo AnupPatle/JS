b4fa230c72b96109795b67612e211c274a46bb81
Anup Patle <anuppatle22110@gmail.com>
2026-01-22 00:15:12 +0530

    classes in js

---
+++ Week-2-JS/ClassesInJS/dateClass.js
@@ -0,0 +1,7 @@
+const date = new Date();
+console.log(date);
+console.log(date.getTime());
+console.log(date.getFullYear());
+console.log(date.getDay());
+console.log(date.getMonth());
+console.log(date.toDateString());
---
+++ Week-2-JS/ClassesInJS/mapClass.js
@@ -0,0 +1,5 @@
+const map = new Map();
+map.set('name',"anup")
+map.set("age",30)
+console.log(map.get('name'));
+console.log(map.get('age'));
